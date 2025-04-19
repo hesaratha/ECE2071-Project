4f823fcdafeecfacd0f3ef65837c1542dacb8339
wsiv0001 <wsiv0001@student.monash.edu>
2025-04-19 11:42:20 +1000

    The new manual_audio mode with complete functionality has been integrated

--- Host/main.py
+++ Host/main.py
@@ -3,16 +3,22 @@ import serial
 import wave
 import serial.tools.list_ports
 import matplotlib.pyplot as plt
+import numpy as np
 
 SAMPLE_RATE = 5000
 BAUD_RATE = 115200
 BASE_FILENAME = "recording"
-EXTENSION = ".wav"
 FILENAME_FORMAT = "{base}_{index}{ext}"
 
 modeOptions = {
     1: "Manual Recording Mode",
-    2: "Distance Trigger Mode",
+    2: "Distance Trigger Mode"
+}
+
+ouputOptions = {
+    1: ".wav",
+    2: ".png",
+    3: ".csv"
 }
 
 currentChoice = None
@@ -31,30 +37,71 @@ def get_unique_filename(base, ext):
 
 stm_port = find_stm_port()
 
-def display_main_menu():
-    """Displays main menu with available modes
-    no parameters
+def display_menu(menu):
+    """Displays menu with available modes
+    parameters: dictionary to display
     no returns:
     """
-    print("Main Menu")
-    print("-------------------------")
-    for key, value in modeOptions.items():
-        print(f"{key}: {value} Mode")
-    print("Press Ctrl+C to quit")
+    if menu == modeOptions:
+        print("Main Menu")
+        print("-------------------------")
+        for key, value in menu.items():
+            print(f"{key}: {value} Mode")
+        print("Press Ctrl+C to quit")
+    else:
+        print("Select file Type")
+        for key, value in menu.items():
+            print(f"{key}: {value}")
 
 def quit_program():
+    """
+    exits program
+    parameters:
+    returns:
+    """
     print("\nShutting Down...\n")
     exit(0)
 
 def get_sample_Duration():
-    sampleDuration = round(float(input("Sample Duration: ")),3) # round just in case the user asks for some ridiculous precision that exceeds the precision of our sample rate
-    return sampleDuration
+    """
+    gets user input float for sample duration
+    parameters: round(float(input("Sample Duration: ")),3)
+    returns float sample duration to 3dp
+    """
+    while True:
+        try:
+            sampleDuration = round(float(input("Sample Duration: ")),3) # round just in case the user asks for some ridiculous precision that exceeds the precision of our sample rate
+            return sampleDuration
+        except ValueError:
+                print("Error: Length entered was not a number")
+
+def make_wav_file(filename, data):
+    with wave.open(filename, 'wb') as wav:
+        wav.setnchannels(1)
+        wav.setsampwidth(1)
+        wav.setframerate(SAMPLE_RATE)
+        wav.writeframes(data)
+
+def make_png_file(filename, data, numSamples):
+    time = np.arange(0, numSamples)/SAMPLE_RATE
+    amplitude = np.array(data)
+    plt.plot(time,amplitude, linewidth = 0.2)
+    plt.title("Audio Data")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.savefig(filename)
+    #plt.show()
 
 def manual_recording_mode():
-    filename = get_unique_filename(BASE_FILENAME, EXTENSION)
     print("\nMaunual Recording Mode:")
+    
     sampleDuration = get_sample_Duration()
-    numSamples = sampleDuration*SAMPLE_RATE
+    numSamples = int(sampleDuration*SAMPLE_RATE)
+
+    display_menu(ouputOptions)
+    fileType = ouputOptions[get_menu_choice(ouputOptions)]
+    filename = get_unique_filename(BASE_FILENAME, fileType) 
 
     print(f"\nStarting audio recording:")
     print(
@@ -73,22 +120,23 @@ def manual_recording_mode():
             if percent != last_printed_percent:
                 print(f"\rProgress: {percent}%", end="")
                 last_printed_percent = percent
-    print(f"\nAudio recording completed successfully.")
 
+    print(f"\nAudio recording completed successfully.")
     print(f"\nWriting audio data to file: {filename}")
 
-    with wave.open(filename, 'wb') as wav:
-        wav.setnchannels(1)
-        wav.setsampwidth(1)
-        wav.setframerate(SAMPLE_RATE)
-        wav.writeframes(data)
+    if fileType == ".wav":
+        make_wav_file(filename, data)
+    elif fileType == ".png":
+        make_png_file(filename, data, numSamples)
+    else:
+        np.savetxt(filename,data,header=f"Sample rate is: {SAMPLE_RATE} sps")
 
     print("File saved successfully.")
 
 def distance_trigger_mode():
     quit_program() #placeholder
 
-def get_menu_choice() -> int:
+def get_menu_choice(menu) -> int:
     """gets user input for menu choices
     parameters: input(option:)
     no returns
@@ -96,7 +144,7 @@ def get_menu_choice() -> int:
     while True:
         try:
             choice = int(input("Option: "))
-            if choice in modeOptions.keys():
+            if choice in menu.keys():
                 return choice
             else:
                 print("Error: Invalid option")
@@ -120,14 +168,14 @@ def change_mode(choice):
             case 2:
                 distance_trigger_mode()
 
-def run_menu():
+def run_menu(menu):
     """displays main menu
     no parameters
     no returns
     """
     while True:
-        display_main_menu()
-        mode = get_menu_choice()
+        display_menu(menu)
+        mode = get_menu_choice(menu)
         change_mode(mode)
 
 def main():
@@ -140,7 +188,7 @@ def main():
     print(f"Device found on {stm_port}.")
 
     while True:
-        run_menu()
+        run_menu(modeOptions)
 
 if __name__ == "__main__":
     main()
